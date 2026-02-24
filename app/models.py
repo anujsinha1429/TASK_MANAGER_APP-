06e2a6acfbe81feadc77939a98af71c636e01efa
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-02-25 01:00:08 +0530

    Task class added

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(20),unique=True,nullable=False)
     password=db.Column(db.String(100),nullable=False)
+class Task (db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(20))
+    completed=db.Column(db.Boolean,default=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
