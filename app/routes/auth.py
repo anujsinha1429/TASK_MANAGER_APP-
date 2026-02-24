a35aafb7664b8154db564e87957ccf5c5edbf0aa
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-02-25 01:03:02 +0530

    updated register and login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,11 +1,45 @@
-from flask import Blueprint
+from flask import Blueprint,render_template,request,redirect,url_for
+from werkzeug.security import generate_password_hash,check_password_hash
+from app import db
+from app.models import User
+from flask_login import login_user
 
 auth=Blueprint('auth',__name__)
 
-@auth.route("/login")
-def login():
-    return "<h1> Login page </h1>"
-
-@auth.route("/register")
+@auth.route("/register",methods=["GET","POST"])
 def register():
-    return "<h1> Register page </h1>"
+    if request.method=="POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+        existing_user=User.query.filter_by(username=username).first()
+        if existing_user:
+            return "user already exist"
+        hashed_password=generate_password_hash(password)
+
+        new_user=User(username=username,password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("auth.login"))
+    
+    return render_template("register.html")
+
+
+@auth.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+        if not username or not password:
+            return "username and passsword required"
+        user=User.query.filter_by(username=username).first()
+
+        if user is None:
+            return "Invalid username or password "
+        if not check_password_hash(user.password,password):
+             return "incorrect password"
+        
+        login_user(user)
+        return redirect(url_for("task.dashboard"))
+           
+    return render_template("login.html")
