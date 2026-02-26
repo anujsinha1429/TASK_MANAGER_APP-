311b7f81691905e00dd1787be371ea6ba6ae893f
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-02-26 17:29:45 +0530

    Updated model

--- app/models.py
+++ app/models.py
@@ -5,8 +5,9 @@ class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(20),unique=True,nullable=False)
     password=db.Column(db.String(100),nullable=False)
+    task =db.relationship('Task',backref='owner',lazy=True)
 class Task (db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(20))
+    content=db.Column(db.String(20))
     completed=db.Column(db.Boolean,default=False)
-    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
