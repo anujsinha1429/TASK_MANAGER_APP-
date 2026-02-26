56d99dc9016df409268e5671fe774d32ffa3c08c
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-02-27 00:34:34 +0530

    updated to status from completed '

--- app/models.py
+++ app/models.py
@@ -9,5 +9,5 @@ class User(db.Model,UserMixin):
 class Task (db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     content=db.Column(db.String(20))
-    completed=db.Column(db.Boolean,default=False)
+    status=db.Column(db.String(20),default="not_started")
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
